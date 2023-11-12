05c3b6443ca03214159f6af183778e18454e1bdf
Scott Wattenbarger <scottwattenbarger@gmail.com>
2023-11-11 18:08:27 -0800

    change nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Button } from './Button';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,29 +6,20 @@ import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
   const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
 
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  };
-
   useEffect(() => {
-    showButton();
+    const handleResize = () => {
+      // Your resize logic here
+    };
 
     // Add the event listener
-    window.addEventListener('resize', showButton);
+    window.addEventListener('resize', handleResize);
     
     // Cleanup function to remove the event listener
-    return () => {
-      window.removeEventListener('resize', showButton);
-    };
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   return (
@@ -40,16 +30,8 @@ function Navbar() {
             <img src="images/Wattenbarger Events.png" id='logo' alt="logo" />
             Watt Events
           </Link>
-          <div 
-            className='menu-icon' 
-            onClick={handleClick}
-            role="button" 
-            aria-label="Toggle Navigation Menu"
-            aria-expanded={click}
-          >
-            <FontAwesomeIcon icon={click ? faTimes : faBars}
-                className={click ? "faTimes" : "faBars"}
-                />
+          <div className='menu-icon' onClick={handleClick}>
+            <FontAwesomeIcon icon={click ? faTimes : faBars} />
           </div>
           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             <li className='nav-item'>
@@ -58,34 +40,21 @@ function Navbar() {
               </Link>
             </li>
             <li className='nav-item'>
-              <Link
-                to='/services'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
+              <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
                 Services
               </Link>
             </li>
             <li className='nav-item'>
-              <Link
-                to='/products'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
+              <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
                 Products
               </Link>
             </li>
-            <li>
-              <Link
-                to='/sign-up'
-                className='nav-links-mobile'
-                onClick={closeMobileMenu}
-              >
+            <li className='nav-item'>
+              <Link to='/sign-up' className='nav-links' onClick={closeMobileMenu}>
                 Contact
               </Link>
             </li>
           </ul>
-          {button && <Button buttonStyle='btn--outline'>Contact</Button>}
         </div>
       </nav>
     </>
